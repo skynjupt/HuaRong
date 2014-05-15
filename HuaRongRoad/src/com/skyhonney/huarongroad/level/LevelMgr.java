5a9c009f278f7e0303578aa3aff5e1cbe31a466a
harry <harry@harry-VirtualBox.(none)>
2014-05-15 19:18:45 +0800

    add level manage

--- HuaRongRoad/src/com/skyhonney/huarongroad/level/LevelMgr.java
+++ HuaRongRoad/src/com/skyhonney/huarongroad/level/LevelMgr.java
@@ -1,5 +1,9 @@
 package com.skyhonney.huarongroad.level;
 
+import java.util.List;
+
+import android.content.Context;
+
 public class LevelMgr {
 	private static final String HEIGHT_TAG = "height";
 	private static final String WIDTH_TAG = "width";
@@ -7,7 +11,292 @@ public class LevelMgr {
 	private static final String Y_TAG = "y";
 	private static final String PEPOLEDATA_TAG = "PepoleData";
 	private static final String PEPOLE_TYPE_TAG = "pepoleType";
-	
-	
-	
+
+	public static void createPepoleComps(List<PepoleData> list, Context c) {
+
+	}
+
+	public static void genLevel(int level, Context c) {
+		createPepoleComps(getLevel(level, c), c);
+	}
+
+	public static List<PepoleData> getLevel(int level, Context context) {
+
+		List list = null;
+
+		switch (level) {
+		case LevelType.tao_zhi_yao_yao:
+			list = parseXML("level/tao_zhi_yao_yao.xml", context);
+			break;
+		case LevelType.wu_heng_zhi_ju:
+			list = parseXML("level/wu_heng_zhi_ju.xml", context);
+			break;
+		case LevelType.jiang_dang_hou_lu:
+			list = parseXML("level/jiang_dang_hou_lu.xml", context);
+			break;
+		case LevelType.qian_hu_hou_yong:
+			list = parseXML("level/qian_hu_hou_yong.xml", context);
+			break;
+		case LevelType.bi_yi_heng_kong:
+			list = parseXML("level/bi_yi_heng_kong.xml", context);
+			break;
+		case LevelType.qiao_guo_wu_guan:
+			list = parseXML("level/qiao_guo_wu_guan.xml", context);
+			break;
+		case LevelType.wu_jiang_bi_gong:
+			list = parseXML("level/wu_jiang_bi_gong.xml", context);
+			break;
+		case LevelType.bing_lin_cao_ying:
+			list = parseXML("level/bing_lin_cao_ying.xml", context);
+			break;
+		case LevelType.si_jiang_lian_guan:
+			list = parseXML("level/si_jiang_lian_guan.xml", context);
+			break;
+		case LevelType.xin_jin_zai_zhi_chi:
+			list = parseXML("level/xin_jin_zai_zhi_chi.xml", context);
+			break;
+		case LevelType.xing_luo_qi_bu:
+			list = parseXML("level/xing_luo_qi_bu.xml", context);
+			break;
+		case LevelType.si_mian_ba_fang:
+			list = parseXML("level/si_mian_ba_fang.xml", context);
+			break;
+		case LevelType.niu_qi_chong_tian:
+			list = parseXML("level/niu_qi_chong_tian.xml", context);
+			break;
+		case LevelType.diao_bing_qian_jiang:
+			list = parseXML("level/diao_bing_qian_jiang.xml", context);
+			break;
+		case LevelType.bei_shui_lie_zhen:
+			list = parseXML("level/bei_shui_lie_zhen.xml", context);
+			break;
+		case LevelType.jie_jie_gao_shen:
+			list = parseXML("level/jie_jie_gao_shen.xml", context);
+			break;
+		case LevelType.lun_hui:
+			list = parseXML("level/lun_hui.xml", context);
+			break;
+		case LevelType.si_mian_chu_ge:
+			list = parseXML("level/si_mian_chu_ge.xml", context);
+			break;
+		case LevelType.cha_chi_nan_fei:
+			list = parseXML("level/cha_chi_nan_fei.xml", context);
+			break;
+		case LevelType.wu_shu_zhi_ju:
+			list = parseXML("level/wu_shu_zhi_ju.xml", context);
+			break;
+		case LevelType.wei_er_bu_jian:
+			list = parseXML("level/wei_er_bu_jian.xml", context);
+			break;
+		case LevelType.fang_jing_di_zhi_wa:
+			list = parseXML("level/fang_jing_di_zhi_wa.xml", context);
+			break;
+		case LevelType.yi_lu_jin_jun:
+			list = parseXML("level/yi_lu_jin_jun.xml", context);
+			break;
+		case LevelType.fa_ma:
+			list = parseXML("level/fa_ma.xml", context);
+			break;
+		case LevelType.yi_fu_dang_guan:
+			list = parseXML("level/yi_fu_dang_guan.xml", context);
+			break;
+		case LevelType.qi_tou_bing_jin:
+			list = parseXML("level/qi_tou_bing_jin.xml", context);
+			break;
+		case LevelType.jing_di_zhi_wa:
+			list = parseXML("level/jing_di_zhi_wa.xml", context);
+			break;
+		case LevelType.heng_dao_li_ma_3:
+			list = parseXML("level/heng_dao_li_ma_3.xml", context);
+			break;
+		case LevelType.shu_tu_tong_gui_2:
+			list = parseXML("level/shu_tu_tong_gui_2.xml", context);
+			break;
+		case LevelType.san_jun_lian_fang:
+			list = parseXML("level/san_jun_lian_fang.xml", context);
+			break;
+		case LevelType.si_lu_jin_bing:
+			list = parseXML("level/si_lu_jin_bing.xml", context);
+			break;
+		case LevelType.chong_chong_bao_wei:
+			list = parseXML("level/chong_chong_bao_wei.xml", context);
+			break;
+		case LevelType.lie_dui_ying_jie:
+			list = parseXML("level/lie_dui_ying_jie.xml", context);
+			break;
+		case LevelType.jiang_yong_cao_yin:
+			list = parseXML("level/jiang_yong_cao_yin.xml", context);
+			break;
+		case LevelType.jiang_shou_jiao_lou:
+			list = parseXML("level/jiang_shou_jiao_lou.xml", context);
+			break;
+		case LevelType.heng_dao_li_ma_2:
+			list = parseXML("level/heng_dao_li_ma_2.xml", context);
+			break;
+		case LevelType.tao_hua_yuan_zhong:
+			list = parseXML("level/tao_hua_yuan_zhong.xml", context);
+			break;
+		case LevelType.pi_ma_si_feng:
+			list = parseXML("level/pi_ma_si_feng.xml", context);
+			break;
+		case LevelType.yi_ru_qi_tu:
+			list = parseXML("level/yi_ru_qi_tu.xml", context);
+			break;
+		case LevelType.zuo_bing_you_jiang:
+			list = parseXML("level/zuo_bing_you_jiang.xml", context);
+			break;
+		case LevelType.yun_zhe_wu_zhao:
+			list = parseXML("level/yun_zhe_wu_zhao.xml", context);
+			break;
+		case LevelType.heng_shu_jie_jiang:
+			list = parseXML("level/heng_shu_jie_jiang.xml", context);
+			break;
+		case LevelType.yi_di_tong_xin:
+			list = parseXML("level/yi_di_tong_xin.xml", context);
+			break;
+		case LevelType.shu_tu_tong_gui:
+			list = parseXML("level/shu_tu_tong_gui.xml", context);
+			break;
+		case LevelType.jia_dao_cang_bing:
+			list = parseXML("level/jia_dao_cang_bing.xml", context);
+			break;
+		case LevelType.yu_qiao_yu_zhuo:
+			list = parseXML("level/yu_qiao_yu_zhuo.xml", context);
+			break;
+		case LevelType.liu_jinag_shou_guan:
+			list = parseXML("level/liu_jinag_shou_guan.xml", context);
+			break;
+		case LevelType.shou_kou_ru_ping_1:
+			list = parseXML("level/shou_kou_ru_ping_1.xml", context);
+			break;
+		case LevelType.shui_xie_bu_tong:
+			list = parseXML("level/shui_xie_bu_tong.xml", context);
+			break;
+		case LevelType.du_pi_xi_jing:
+			list = parseXML("level/du_pi_xi_jing.xml", context);
+			break;
+		case LevelType.heng_dao_li_ma_1:
+			list = parseXML("level/heng_dao_li_ma_1.xml", context);
+			break;
+		case LevelType.huan_li_wei_dui:
+			list = parseXML("level/huan_li_wei_dui.xml", context);
+			break;
+		case LevelType.bing_dang_jiang_zu:
+			list = parseXML("level/bing_dang_jiang_zu.xml", context);
+			break;
+		case LevelType.da_du_qiao_heng:
+			list = parseXML("level/da_du_qiao_heng.xml", context);
+			break;
+		case LevelType.ru_di_wu_men:
+			list = parseXML("level/ru_di_wu_men.xml", context);
+			break;
+		case LevelType.web_wang_ba_gua:
+			list = parseXML("level/web_wang_ba_gua.xml", context);
+			break;
+		case LevelType.xing_bai_li_zhe:
+			list = parseXML("level/xing_bai_li_zhe.xml", context);
+			break;
+		case LevelType.yu_ba_bu_neng:
+			list = parseXML("level/yu_ba_bu_neng.xml", context);
+			break;
+		case LevelType.shou_kou_ru_ping_2:
+			list = parseXML("level/shou_kou_ru_ping_2.xml", context);
+			break;
+		case LevelType.yang_96:
+			list = parseXML("level/yang_96.xml", context);
+			break;
+		case LevelType.san_yang_kai_tai:
+			list = parseXML("level/san_yang_kai_tai.xml", context);
+			break;
+		case LevelType.si_bing_tong_xin:
+			list = parseXML("level/si_bing_tong_xin.xml", context);
+			break;
+		case LevelType.yin_96:
+			list = parseXML("level/yin_96.xml", context);
+			break;
+		case LevelType.jin_zai_zhi_chi_2:
+			list = parseXML("level/jin_zai_zhi_chi_2.xml", context);
+			break;
+		case LevelType.xiang_kan_liang_bu_yan:
+			list = parseXML("level/xiang_kan_liang_bu_yan.xml", context);
+			break;
+		case LevelType.yu_you_chun_shui:
+			list = parseXML("level/yu_you_chun_shui.xml", context);
+			break;
+		case LevelType.bai_bu_qi_xing:
+			list = parseXML("level/bai_bu_qi_xing.xml", context);
+			break;
+		case LevelType.shen_xian_ling_yu:
+			list = parseXML("level/shen_xian_ling_yu.xml", context);
+			break;
+		case LevelType.an_du_chen_cang:
+			list = parseXML("level/an_du_chen_cang.xml", context);
+			break;
+		case LevelType.xiao_yan_chu_chao:
+			list = parseXML("level/xiao_yan_chu_chao.xml", context);
+			break;
+		case LevelType.xu_wu_piao_miao:
+			list = parseXML("level/xu_wu_piao_miao.xml", context);
+			break;
+		case LevelType.heng_xing_zhi_jiang:
+			list = parseXML("level/heng_xing_zhi_jiang.xml", context);
+			break;
+		case LevelType.jin_zai_zhi_chi_1:
+			list = parseXML("level/jin_zai_zhi_chi_1.xml", context);
+			break;
+		case LevelType.ceng_ceng_she_fang_1:
+			list = parseXML("level/ceng_ceng_she_fang_1.xml", context);
+			break;
+		case LevelType.shui_hu_ju_yi:
+			list = parseXML("level/shui_hu_ju_yi.xml", context);
+			break;
+		case LevelType.qian_jia_hou_gong:
+			list = parseXML("level/qian_jia_hou_gong.xml", context);
+			break;
+		case LevelType.feng_jing_ma_liang_2:
+			list = parseXML("level/feng_jing_ma_liang_2.xml", context);
+			break;
+		case LevelType.xi_ri_ba_zhu:
+			list = parseXML("level/xi_ri_ba_zhu.xml", context);
+			break;
+		case LevelType.dan_shen_xiao_bing:
+			list = parseXML("level/dan_shen_xiao_bing.xml", context);
+			break;
+		case LevelType.sheng_ji_ang_ran:
+			list = parseXML("level/sheng_ji_ang_ran.xml", context);
+			break;
+		case LevelType.chang_tu_ba_she:
+			list = parseXML("level/chang_tu_ba_she.xml", context);
+			break;
+		case LevelType.tun_bing_dong_lu:
+			list = parseXML("level/tun_bing_dong_lu.xml", context);
+			break;
+		case LevelType.quan_tou:
+			list = parseXML("level/quan_tou.xml", context);
+			break;
+		case LevelType.feng_jing_ma_liang_1:
+			list = parseXML("level/feng_jing_ma_liang_1.xml", context);
+			break;
+		case LevelType.ceng_ceng_she_fang_2:
+			list = parseXML("level/ceng_ceng_she_fang_2.xml", context);
+			break;
+		case LevelType.bing_jiang_lian_fang:
+			list = parseXML("level/bing_jiang_lian_fang.xml", context);
+			break;
+		case LevelType.xiao_bin_tan_lu:
+			list = parseXML("level/xiao_bin_tan_lu.xml", context);
+			break;
+		case LevelType.feng_hui_lu_zhuan:
+			list = parseXML("level/feng_hui_lu_zhuan.xml", context);
+			break;
+
+		}
+
+		return list;
+	}
+
+	public static List<PepoleData> parseXML(String path, Context c) {
+		return null;
+	}
+
 }
--- HuaRongRoad/src/com/skyhonney/huarongroad/level/LevelType.java
+++ HuaRongRoad/src/com/skyhonney/huarongroad/level/LevelType.java
@@ -1,5 +1,372 @@
 package com.skyhonney.huarongroad.level;
 
+import com.skyhonney.huarongroad.R;
+
 public class LevelType {
+	public static final int tao_zhi_yao_yao = 0;
+	public static final int wu_heng_zhi_ju = 1;
+	public static final int jiang_dang_hou_lu = 2;
+	public static final int qian_hu_hou_yong = 3;
+	public static final int bi_yi_heng_kong = 4;
+	public static final int qiao_guo_wu_guan = 5;
+	public static final int wu_jiang_bi_gong = 6;
+	public static final int bing_lin_cao_ying = 7;
+	public static final int si_jiang_lian_guan = 8;
+	public static final int xin_jin_zai_zhi_chi = 9;
+	public static final int xing_luo_qi_bu = 10;
+	public static final int si_mian_ba_fang = 11;
+	public static final int niu_qi_chong_tian = 12;
+	public static final int diao_bing_qian_jiang = 13;
+	public static final int bei_shui_lie_zhen = 14;
+	public static final int jie_jie_gao_shen = 15;
+	public static final int lun_hui = 16;
+	public static final int si_mian_chu_ge = 17;
+	public static final int cha_chi_nan_fei = 18;
+	public static final int wu_shu_zhi_ju = 19;
+	public static final int wei_er_bu_jian = 20;
+	public static final int fang_jing_di_zhi_wa = 21;
+	public static final int yi_lu_jin_jun = 22;
+	public static final int fa_ma = 23;
+	public static final int yi_fu_dang_guan = 24;
+	public static final int qi_tou_bing_jin = 25;
+	public static final int jing_di_zhi_wa = 26;
+	public static final int heng_dao_li_ma_3 = 27;
+	public static final int shu_tu_tong_gui_2 = 28;
+	public static final int san_jun_lian_fang = 29;
+	public static final int si_lu_jin_bing = 30;
+	public static final int chong_chong_bao_wei = 31;
+	public static final int lie_dui_ying_jie = 32;
+	public static final int jiang_yong_cao_yin = 33;
+	public static final int jiang_shou_jiao_lou = 34;
+	public static final int heng_dao_li_ma_2 = 35;
+	public static final int tao_hua_yuan_zhong = 36;
+	public static final int pi_ma_si_feng = 37;
+	public static final int yi_ru_qi_tu = 38;
+	public static final int zuo_bing_you_jiang = 39;
+	public static final int yun_zhe_wu_zhao = 40;
+	public static final int heng_shu_jie_jiang = 41;
+	public static final int yi_di_tong_xin = 42;
+	public static final int shu_tu_tong_gui = 43;
+	public static final int jia_dao_cang_bing = 44;
+	public static final int yu_qiao_yu_zhuo = 45;
+	public static final int liu_jinag_shou_guan = 46;
+	public static final int shou_kou_ru_ping_1 = 47;
+	public static final int shui_xie_bu_tong = 48;
+	public static final int du_pi_xi_jing = 49;
+	public static final int heng_dao_li_ma_1 = 50;
+	public static final int huan_li_wei_dui = 51;
+	public static final int bing_dang_jiang_zu = 52;
+	public static final int da_du_qiao_heng = 53;
+	public static final int ru_di_wu_men = 54;
+	public static final int web_wang_ba_gua = 55;
+	public static final int xing_bai_li_zhe = 56;
+	public static final int yu_ba_bu_neng = 57;
+	public static final int shou_kou_ru_ping_2 = 58;
+	public static final int yang_96 = 59;
+	public static final int san_yang_kai_tai = 60;
+	public static final int si_bing_tong_xin = 61;
+	public static final int yin_96 = 62;
+	public static final int jin_zai_zhi_chi_2 = 63;
+	public static final int xiang_kan_liang_bu_yan = 64;
+	public static final int yu_you_chun_shui = 65;
+	public static final int bai_bu_qi_xing = 66;
+	public static final int shen_xian_ling_yu = 67;
+	public static final int an_du_chen_cang = 68;
+	public static final int xiao_yan_chu_chao = 69;
+	public static final int xu_wu_piao_miao = 70;
+	public static final int heng_xing_zhi_jiang = 71;
+	public static final int jin_zai_zhi_chi_1 = 72;
+	public static final int ceng_ceng_she_fang_1 = 73;
+	public static final int shui_hu_ju_yi = 74;
+	public static final int qian_jia_hou_gong = 75;
+	public static final int feng_jing_ma_liang_2 = 76;
+	public static final int xi_ri_ba_zhu = 77;
+	public static final int dan_shen_xiao_bing = 78;
+	public static final int sheng_ji_ang_ran = 79;
+	public static final int chang_tu_ba_she = 80;
+	public static final int tun_bing_dong_lu = 81;
+	public static final int quan_tou = 82;
+	public static final int feng_jing_ma_liang_1 = 83;
+	public static final int ceng_ceng_she_fang_2 = 84;
+	public static final int bing_jiang_lian_fang = 85;
+	public static final int xiao_bin_tan_lu = 86;
+	public static final int feng_hui_lu_zhuan = 87;
+	public static final int count = 88;
+
+	public static int getLevelName(int level) {
+
+		int stringId;
+
+		switch (level) {
+		case tao_zhi_yao_yao:
+			stringId = R.string.tao_zhi_yao_yao;
+			break;
+		case wu_heng_zhi_ju:
+			stringId = R.string.wu_heng_zhi_ju;
+			break;
+		case jiang_dang_hou_lu:
+			stringId = R.string.jiang_dang_hou_lu;
+			break;
+		case qian_hu_hou_yong:
+			stringId = R.string.qian_hu_hou_yong;
+			break;
+		case bi_yi_heng_kong:
+			stringId = R.string.bi_yi_heng_kong;
+			break;
+		case qiao_guo_wu_guan:
+			stringId = R.string.qiao_guo_wu_guan;
+			break;
+		case wu_jiang_bi_gong:
+			stringId = R.string.wu_jiang_bi_gong;
+			break;
+		case bing_lin_cao_ying:
+			stringId = R.string.bing_lin_cao_ying;
+			break;
+		case si_jiang_lian_guan:
+			stringId = R.string.si_jiang_lian_guan;
+			break;
+		case xin_jin_zai_zhi_chi:
+			stringId = R.string.xin_jin_zai_zhi_chi;
+			break;
+		case xing_luo_qi_bu:
+			stringId = R.string.xing_luo_qi_bu;
+			break;
+		case si_mian_ba_fang:
+			stringId = R.string.si_mian_ba_fang;
+			break;
+		case niu_qi_chong_tian:
+			stringId = R.string.niu_qi_chong_tian;
+			break;
+		case diao_bing_qian_jiang:
+			stringId = R.string.diao_bing_qian_jiang;
+			break;
+		case bei_shui_lie_zhen:
+			stringId = R.string.bei_shui_lie_zhen;
+			break;
+		case jie_jie_gao_shen:
+			stringId = R.string.jie_jie_gao_shen;
+			break;
+		case lun_hui:
+			stringId = R.string.lun_hui;
+			break;
+		case si_mian_chu_ge:
+			stringId = R.string.si_mian_chu_ge;
+			break;
+		case cha_chi_nan_fei:
+			stringId = R.string.cha_chi_nan_fei;
+			break;
+		case wu_shu_zhi_ju:
+			stringId = R.string.wu_shu_zhi_ju;
+			break;
+		case wei_er_bu_jian:
+			stringId = R.string.wei_er_bu_jian;
+			break;
+		case fang_jing_di_zhi_wa:
+			stringId = R.string.fang_jing_di_zhi_wa;
+			break;
+		case yi_lu_jin_jun:
+			stringId = R.string.yi_lu_jin_jun;
+			break;
+		case fa_ma:
+			stringId = R.string.fa_ma;
+			break;
+		case yi_fu_dang_guan:
+			stringId = R.string.yi_fu_dang_guan;
+			break;
+		case qi_tou_bing_jin:
+			stringId = R.string.qi_tou_bing_jin;
+			break;
+		case jing_di_zhi_wa:
+			stringId = R.string.jing_di_zhi_wa;
+			break;
+		case heng_dao_li_ma_3:
+			stringId = R.string.heng_dao_li_ma_3;
+			break;
+		case shu_tu_tong_gui_2:
+			stringId = R.string.shu_tu_tong_gui_2;
+			break;
+		case san_jun_lian_fang:
+			stringId = R.string.san_jun_lian_fang;
+			break;
+		case si_lu_jin_bing:
+			stringId = R.string.si_lu_jin_bing;
+			break;
+		case chong_chong_bao_wei:
+			stringId = R.string.chong_chong_bao_wei;
+			break;
+		case lie_dui_ying_jie:
+			stringId = R.string.lie_dui_ying_jie;
+			break;
+		case jiang_yong_cao_yin:
+			stringId = R.string.jiang_yong_cao_yin;
+			break;
+		case jiang_shou_jiao_lou:
+			stringId = R.string.jiang_shou_jiao_lou;
+			break;
+		case heng_dao_li_ma_2:
+			stringId = R.string.heng_dao_li_ma_2;
+			break;
+		case tao_hua_yuan_zhong:
+			stringId = R.string.tao_hua_yuan_zhong;
+			break;
+		case pi_ma_si_feng:
+			stringId = R.string.pi_ma_si_feng;
+			break;
+		case yi_ru_qi_tu:
+			stringId = R.string.yi_ru_qi_tu;
+			break;
+		case zuo_bing_you_jiang:
+			stringId = R.string.zuo_bing_you_jiang;
+			break;
+		case yun_zhe_wu_zhao:
+			stringId = R.string.yun_zhe_wu_zhang;
+			break;
+		case heng_shu_jie_jiang:
+			stringId = R.string.heng_shu_jie_jiang;
+			break;
+		case yi_di_tong_xin:
+			stringId = R.string.yi_di_tong_xin;
+			break;
+		case shu_tu_tong_gui:
+			stringId = R.string.shu_tu_tong_gui;
+			break;
+		case jia_dao_cang_bing:
+			stringId = R.string.jia_dao_cang_bing;
+			break;
+		case yu_qiao_yu_zhuo:
+			stringId = R.string.yu_qiao_yu_zhuo;
+			break;
+		case liu_jinag_shou_guan:
+			stringId = R.string.liu_jinag_shou_guan;
+			break;
+		case shou_kou_ru_ping_1:
+			stringId = R.string.shou_kou_ru_ping;
+			break;
+		case shui_xie_bu_tong:
+			stringId = R.string.shui_xie_bu_tong;
+			break;
+		case du_pi_xi_jing:
+			stringId = R.string.du_pi_xi_jing;
+			break;
+		case heng_dao_li_ma_1:
+			stringId = R.string.heng_dao_li_ma;
+			break;
+		case huan_li_wei_dui:
+			stringId = R.string.huan_li_wei_dui;
+			break;
+		case bing_dang_jiang_zu:
+			stringId = R.string.bing_dang_jiang_zu;
+			break;
+		case da_du_qiao_heng:
+			stringId = R.string.da_du_qiao_heng;
+			break;
+		case ru_di_wu_men:
+			stringId = R.string.ru_di_wu_men;
+			break;
+		case web_wang_ba_gua:
+			stringId = R.string.web_wang_ba_gua;
+			break;
+		case xing_bai_li_zhe:
+			stringId = R.string.xing_bai_li_zhe;
+			break;
+		case yu_ba_bu_neng:
+			stringId = R.string.yu_ba_bu_neng;
+			break;
+		case shou_kou_ru_ping_2:
+			stringId = R.string.shou_kou_ru_ping_1;
+			break;
+		case yang_96:
+			stringId = R.string.yang_96;
+			break;
+		case san_yang_kai_tai:
+			stringId = R.string.san_yang_kai_tai;
+			break;
+		case si_bing_tong_xin:
+			stringId = R.string.si_bing_tong_xin;
+			break;
+		case yin_96:
+			stringId = R.string.yin_96;
+			break;
+		case jin_zai_zhi_chi_2:
+			stringId = R.string.jin_zai_zhi_chi_2;
+			break;
+		case xiang_kan_liang_bu_yan:
+			stringId = R.string.xiang_kan_liang_bu_yan;
+			break;
+		case yu_you_chun_shui:
+			stringId = R.string.yu_you_chun_shui;
+			break;
+		case bai_bu_qi_xing:
+			stringId = R.string.bai_bu_qi_xing;
+			break;
+		case shen_xian_ling_yu:
+			stringId = R.string.shen_xian_ling_yu;
+			break;
+		case an_du_chen_cang:
+			stringId = R.string.an_du_chen_cang;
+			break;
+		case xiao_yan_chu_chao:
+			stringId = R.string.xiao_yan_chu_chao;
+			break;
+		case xu_wu_piao_miao:
+			stringId = R.string.xu_wu_piao_miao;
+			break;
+		case heng_xing_zhi_jiang:
+			stringId = R.string.heng_xing_zhi_jiang;
+			break;
+		case jin_zai_zhi_chi_1:
+			stringId = R.string.jin_zai_zhi_chi_1;
+			break;
+		case ceng_ceng_she_fang_1:
+			stringId = R.string.ceng_ceng_she_fang;
+			break;
+		case shui_hu_ju_yi:
+			stringId = R.string.shui_hu_ju_yi;
+			break;
+		case qian_jia_hou_gong:
+			stringId = R.string.qian_jia_hou_gong;
+			break;
+		case feng_jing_ma_liang_2:
+			stringId = R.string.feng_jing_ma_liang_2;
+			break;
+		case xi_ri_ba_zhu:
+			stringId = R.string.xi_ri_ba_zhu;
+			break;
+		case dan_shen_xiao_bing:
+			stringId = R.string.dan_shen_xiao_bing;
+			break;
+		case sheng_ji_ang_ran:
+			stringId = R.string.sheng_ji_ang_ran;
+			break;
+		case chang_tu_ba_she:
+			stringId = R.string.chang_tu_ba_she;
+			break;
+		case tun_bing_dong_lu:
+			stringId = R.string.tun_bing_dong_lu;
+			break;
+		case quan_tou:
+			stringId = R.string.quan_tou;
+			break;
+		case feng_jing_ma_liang_1:
+			stringId = R.string.feng_jing_ma_liang_1;
+			break;
+		case ceng_ceng_she_fang_2:
+			stringId = R.string.ceng_ceng_she_fang_2;
+			break;
+		case bing_jiang_lian_fang:
+			stringId = R.string.bing_jiang_lian_fang;
+			break;
+		case xiao_bin_tan_lu:
+			stringId = R.string.xiao_bin_tan_lu;
+			break;
+		case feng_hui_lu_zhuan:
+			stringId = R.string.feng_hui_lu_zhuan;
+			break;
+		default:
+			stringId = 0;
+		}
+		
+		return stringId;
+	}
 
 }
